4f2bf1b9d50df1eae77f02fd4a40437e3adecbce
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2022-06-16 22:53:46 +0800

    minor fix

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -13,7 +13,6 @@
 #include <fmt/core.h>
 #include <mio/mmap.hpp>
 
-#include <cstddef>
 #include <vector>
 
 namespace lexer {
@@ -51,8 +50,7 @@ enum class symbol_t {
   period,
 };
 
-[[nodiscard]] constexpr boost::string_view
-stringify_symbol(symbol_t sym) noexcept {
+[[nodiscard]] constexpr boost::string_view stringify_symbol(symbol_t sym) noexcept {
   using enum symbol_t;
   // NOLINTNEXTLINE
   constexpr boost::string_view strings[] = {
@@ -92,9 +90,7 @@ stringify_symbol(symbol_t sym) noexcept {
   return strings[static_cast<std::size_t>(sym)];
 }
 
-inline std::ostream& operator<<(std::ostream& os, symbol_t sym) {
-  return os << stringify_symbol(sym);
-}
+inline std::ostream& operator<<(std::ostream& os, symbol_t sym) { return os << stringify_symbol(sym); }
 
 namespace detail {
 using read_result_t = boost::optional<std::pair<char const*, std::size_t>>;
@@ -125,77 +121,57 @@ struct lex_error_missing_ending_period_t {
   boost::string_view path;
 };
 using tokens_t = std::vector<token_t>;
-using lex_result_t =
-    boost::variant2::variant<tokens_t, lex_error_file_unreadable_t>;
+using lex_result_t = boost::variant2::variant<tokens_t, lex_error_file_unreadable_t>;
 
 namespace detail {
-[[nodiscard]] inline boost::optional<char> peek_nth_char(char const* content,
-                                                         std::size_t size,
-                                                         std::size_t cur_pos,
-                                                         std::size_t nth) {
-  if (cur_pos + nth >= size) {
+struct cursor_t {
+  char const* content;
+  std::size_t size;
+  std::size_t cur_pos;
+};
+
+[[nodiscard]] inline boost::optional<char> peek_nth_char(cursor_t const& cursor, std::size_t nth) {
+  if (cursor.cur_pos + nth >= cursor.size) {
     return boost::none;
   }
-  return content[cur_pos + nth];
+  return cursor.content[cursor.cur_pos + nth];
 }
 
-[[nodiscard]] inline boost::optional<char>
-peek_cur_char(char const* content, std::size_t size, std::size_t cur_pos) {
-  return peek_nth_char(content, size, cur_pos, 0);
-}
+[[nodiscard]] inline boost::optional<char> peek_cur_char(cursor_t const& cursor) { return peek_nth_char(cursor, 0); }
 
-[[nodiscard]] inline bool try_match_nth_char(char const* content,
-                                             std::size_t size,
-                                             std::size_t cur_pos,
-                                             std::size_t nth, char expected) {
-  if (auto const pc = peek_nth_char(content, size, cur_pos, nth); pc) {
+// TODO(mx): need strong type
+[[nodiscard]] inline bool try_match_nth_char(cursor_t const& cursor, std::size_t nth, char expected) {
+  if (auto const pc = peek_nth_char(cursor, nth); pc) {
     return *pc == expected;
   }
   return false;
 }
 
-[[nodiscard]] inline boost::string_view
-get_number(char const* content, std::size_t size, std::size_t cur_pos) {
-  auto const* const it = boost::algorithm::find_if_not(
-      content + cur_pos, content + size, utils::isdigit_s);
-  return {content + cur_pos,
-          static_cast<std::size_t>(it - (content + cur_pos))};
+[[nodiscard]] inline boost::string_view get_number(cursor_t const& cursor) {
+  auto const* const it =
+      boost::algorithm::find_if_not(cursor.content + cursor.cur_pos, cursor.content + cursor.size, utils::isdigit_s);
+  return {cursor.content + cursor.cur_pos, static_cast<std::size_t>(it - (cursor.content + cursor.cur_pos))};
 }
 
-[[nodiscard]] inline boost::optional<symbol_t>
-try_match_keyword(boost::string_view ident) {
-  static constexpr auto keywords = std::array{
-      std::pair(boost::string_view("call"), symbol_t::call),
-      std::pair(boost::string_view("odd"), symbol_t::odd),
-      std::pair(boost::string_view("begin"), symbol_t::begin),
-      std::pair(boost::string_view("end"), symbol_t::end),
-      std::pair(boost::string_view("if"), symbol_t::if_),
-      std::pair(boost::string_view("while"), symbol_t::while_),
-      std::pair(boost::string_view("then"), symbol_t::then),
-      std::pair(boost::string_view("do"), symbol_t::do_),
-      std::pair(boost::string_view("const"), symbol_t::const_),
-      std::pair(boost::string_view("var"), symbol_t::var),
-      std::pair(boost::string_view("procedure"), symbol_t::proc),
+[[nodiscard]] inline boost::optional<symbol_t> try_match_keyword(boost::string_view ident) {
+  static auto const keywords = boost::unordered_map<boost::string_view, symbol_t>{
+      {"call", symbol_t::call},    {"odd", symbol_t::odd},      {"begin", symbol_t::begin},    {"end", symbol_t::end},
+      {"if", symbol_t::if_},       {"while", symbol_t::while_}, {"then", symbol_t::then},      {"do", symbol_t::do_},
+      {"const", symbol_t::const_}, {"var", symbol_t::var},      {"procedure", symbol_t::proc},
   };
-  if (auto const* const it =
-          std::find_if(keywords.cbegin(), keywords.cend(),
-                       [ident](auto const& p) { return p.first == ident; });
-      it != keywords.cend()) {
+  if (auto const it = keywords.find(ident); it != keywords.cend()) {
     return it->second;
   }
   return boost::none;
 }
 
-[[nodiscard]] inline boost::string_view
-get_identifier(char const* content, std::size_t size, std::size_t cur_pos) {
-  auto const* const it = boost::algorithm::find_if_not(
-      content + cur_pos, content + size, utils::isident_s);
-  return {content + cur_pos,
-          static_cast<std::size_t>(it - (content + cur_pos))};
+[[nodiscard]] inline boost::string_view get_identifier(cursor_t const& cursor) {
+  auto const* const it =
+      boost::algorithm::find_if_not(cursor.content + cursor.cur_pos, cursor.content + cursor.size, utils::isident_s);
+  return {cursor.content + cursor.cur_pos, static_cast<std::size_t>(it - (cursor.content + cursor.cur_pos))};
 }
 
-template <reader_concept T>
-[[nodiscard]] lex_result_t lex(boost::string_view path, T&& reader) {
+template <reader_concept T> [[nodiscard]] lex_result_t lex(boost::string_view path, T&& reader) {
   auto const possible_ret = std::forward<T>(reader)(path);
   if (!possible_ret.has_value()) {
     return lex_error_file_unreadable_t{path};
@@ -205,31 +181,23 @@ template <reader_concept T>
 
   auto tokens = tokens_t();
 
-  auto const add_single_char_token =
-      [&tokens, content = content](std::size_t cur_pos,
-                                   auto sym) -> std::size_t {
+  auto const add_single_char_token = [&tokens, content = content](std::size_t cur_pos, auto sym) -> std::size_t {
     tokens.emplace_back(annotation_t{&content[cur_pos], 1}, sym);
     return 1U;
   };
-  auto const add_two_chars_token =
-      [&tokens, content = content](std::size_t cur_pos,
-                                   auto sym) -> std::size_t {
+  auto const add_two_chars_token = [&tokens, content = content](std::size_t cur_pos, auto sym) -> std::size_t {
     tokens.emplace_back(annotation_t{&content[cur_pos], 2}, sym);
     return 2U;
   };
-  auto const add_number_token =
-      [&tokens, content = content](std::size_t cur_pos,
-                                   boost::string_view num) -> std::size_t {
-    tokens.emplace_back(annotation_t{&content[cur_pos], num.size()},
-                        symbol_t::number);
+  auto const add_number_token = [&tokens, content = content](std::size_t cur_pos,
+                                                             boost::string_view num) -> std::size_t {
+    tokens.emplace_back(annotation_t{&content[cur_pos], num.size()}, symbol_t::number);
     return num.size();
   };
-  auto const add_ident_or_keyword_token =
-      [&tokens, content = content](std::size_t cur_pos,
-                                   boost::string_view ident) -> std::size_t {
+  auto const add_ident_or_keyword_token = [&tokens, content = content](std::size_t cur_pos,
+                                                                       boost::string_view ident) -> std::size_t {
     auto const sym = [ident] {
-      if (auto const possible_keyword = try_match_keyword(ident);
-          possible_keyword) {
+      if (auto const possible_keyword = try_match_keyword(ident); possible_keyword) {
         return *possible_keyword;
       }
       return symbol_t::ident;
@@ -238,66 +206,59 @@ template <reader_concept T>
     return ident.size();
   };
 
-  static auto const single_char_to_token_map =
-      boost::unordered_map<char, symbol_t>{
-          {';', symbol_t::semicolon}, {',', symbol_t::comma},
-          {'(', symbol_t::lparen},    {')', symbol_t::rparen},
-          {'*', symbol_t::times},     {'/', symbol_t::divide},
-          {'+', symbol_t::plus},      {'-', symbol_t::minus},
-          {'=', symbol_t::equal},     {'#', symbol_t::not_equal},
-          {'?', symbol_t::in},        {'!', symbol_t::out},
-          {'.', symbol_t::period},
-      };
+  static auto const single_char_to_token_map = boost::unordered_map<char, symbol_t>{
+      {';', symbol_t::semicolon}, {',', symbol_t::comma},     {'(', symbol_t::lparen}, {')', symbol_t::rparen},
+      {'*', symbol_t::times},     {'/', symbol_t::divide},    {'+', symbol_t::plus},   {'-', symbol_t::minus},
+      {'=', symbol_t::equal},     {'#', symbol_t::not_equal}, {'?', symbol_t::in},     {'!', symbol_t::out},
+      {'.', symbol_t::period},
+  };
 
-  std::size_t cur_pos = 0;
-  while (auto const pc = peek_cur_char(content, size, cur_pos)) {
+  cursor_t cursor{.content = content, .size = size, .cur_pos = 0};
+  while (auto const pc = peek_cur_char(cursor)) {
     if (!pc.has_value()) {
       break;
     }
     char const c = *pc;
-    if (auto const it = single_char_to_token_map.find(c);
-        it != single_char_to_token_map.cend()) {
-      cur_pos += add_single_char_token(cur_pos, it->second);
+    if (auto const it = single_char_to_token_map.find(c); it != single_char_to_token_map.cend()) {
+      cursor.cur_pos += add_single_char_token(cursor.cur_pos, it->second);
     } else if (c == '<') {
-      if (try_match_nth_char(content, size, cur_pos, 1, '=')) {
-        cur_pos += add_two_chars_token(cur_pos, symbol_t::less_equal);
+      if (try_match_nth_char(cursor, 1, '=')) {
+        cursor.cur_pos += add_two_chars_token(cursor.cur_pos, symbol_t::less_equal);
       } else {
-        cur_pos += add_single_char_token(cur_pos, symbol_t::less);
+        cursor.cur_pos += add_single_char_token(cursor.cur_pos, symbol_t::less);
       }
     } else if (c == '>') {
-      if (try_match_nth_char(content, size, cur_pos, 1, '=')) {
-        cur_pos += add_two_chars_token(cur_pos, symbol_t::greater_equal);
+      if (try_match_nth_char(cursor, 1, '=')) {
+        cursor.cur_pos += add_two_chars_token(cursor.cur_pos, symbol_t::greater_equal);
       } else {
-        cur_pos += add_single_char_token(cur_pos, symbol_t::greater);
+        cursor.cur_pos += add_single_char_token(cursor.cur_pos, symbol_t::greater);
       }
       break;
     } else if (c == ':') {
-      if (try_match_nth_char(content, size, cur_pos, 1, '=')) {
-        cur_pos += add_two_chars_token(cur_pos, symbol_t::becomes);
+      if (try_match_nth_char(cursor, 1, '=')) {
+        cursor.cur_pos += add_two_chars_token(cursor.cur_pos, symbol_t::becomes);
       } else {
         fmt::print("unexpected char '{}', should it be ':='?\n", c);
         __builtin_unreachable();
       }
     } else if (utils::isdigit_s(c)) {
-      auto const num = get_number(content, size, cur_pos);
-      cur_pos += add_number_token(cur_pos, num);
+      auto const num = get_number(cursor);
+      cursor.cur_pos += add_number_token(cursor.cur_pos, num);
     } else if (utils::isalpha_s(c)) {
-      auto const ident = get_identifier(content, size, cur_pos);
-      cur_pos += add_ident_or_keyword_token(cur_pos, ident);
+      auto const ident = get_identifier(cursor);
+      cursor.cur_pos += add_ident_or_keyword_token(cursor.cur_pos, ident);
     } else {
       if (!utils::isspace_s(c)) {
         fmt::print("unknown char '{}'\n", c);
       }
-      ++cur_pos;
+      ++cursor.cur_pos;
     }
   }
   return tokens;
 }
 } // namespace detail
 
-[[nodiscard]] inline lex_result_t lex(std::string const& path) {
-  return detail::lex(path, detail::read);
-}
+[[nodiscard]] inline lex_result_t lex(std::string const& path) { return detail::lex(path, detail::read); }
 } // namespace lexer
 
 #endif
