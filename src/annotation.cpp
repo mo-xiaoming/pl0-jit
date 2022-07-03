0a599f3e3f69110076ee382602a9c131dbc3b25d
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2022-07-03 11:32:26 +0800

    too lazy to add line number to error message

--- src/annotation.cpp
+++ src/annotation.cpp
@@ -7,10 +7,8 @@ std::string annotation_to_error_string(annotation_t const& annotation) {
   auto const prev_nl_n = annotation.source.substr(0, annotation.start).rfind('\n');
   auto const prev_nl = (prev_nl_n == std::string_view::npos ? 0 : prev_nl_n);
   auto const next_nl = annotation.source.substr(annotation.start + annotation.length).find('\n');
-  constexpr auto leading_space = 4UL;
-  for (auto i = 0UL; i != leading_space; ++i) {
-    oss << ' ';
-  }
+  constexpr auto leading_space = 8UL;
+  oss << std::setw(leading_space) << "00|";
   oss << annotation.source.substr(prev_nl, next_nl) << '\n';
   for (auto i = 0UL; i != leading_space + annotation.start; ++i) {
     oss << ' ';
